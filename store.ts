50c1f58d9a7fc0e4ac8f4dd128bb11b01a642c8a
Yingtian Shi <shiyt0313@gmail.com>
2023-07-26 03:44:17 +0000

    init all files

---
+++ store.ts
@@ -0,0 +1,9 @@
+import { DataTemp } from "./genie/DataTemp";
+import { HelperTemp } from "./genie/HelperTemp";
+import { initReactGenie } from "reactgenie-lib";
+
+console.log(DataTemp);
+
+export const reactGenieStore = initReactGenie();
+
+//manul set up here
