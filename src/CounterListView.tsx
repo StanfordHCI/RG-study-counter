55239e73557da7610e3dc39520a9e114bf3233a2
Yingtian Shi <shiyt0313@gmail.com>
2023-08-21 05:56:58 +0000

    study user_10

---
+++ src/CounterListView.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { CounterItemView } from "./CounterItemView";
+import {GenieClassInterface} from "reactgenie-lib";
+
+const CounterListViewImpl = (props:{elements: { id: string }[] }) => { //pass the counters to CounterListView
+    const counters =  props.elements
+
+    return (
+        <div>
+            {
+                counters.map((element) => {
+                    return <CounterItemView id={element.id} key={element.id}/>
+                })
+            }
+        </div>
+    )
+}
+
+export const CounterListView = GenieClassInterface("Counter[]", "Counter List")(CounterListViewImpl)
--- store.ts
+++ store.ts
@@ -3,5 +3,3 @@ import { initReactGenie } from "reactgenie-lib";
 
 
 export const reactGenieStore = initReactGenie();
-
-//manul set up here
