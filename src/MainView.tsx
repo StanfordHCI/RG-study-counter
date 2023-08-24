1e635030584df1645748c132dca7b650f913989e
Yingtian Shi <shiyt0313@gmail.com>
2023-08-24 22:19:08 +0000

    study user_12

---
+++ src/MainView.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {View} from "react-native";
+import {CounterListView } from "./CounterListView";
+import {useGenieSelector} from "reactgenie-lib";
+import {Counter} from "../genie/Counter";
+import {AppNavigator} from "../App";
+
+export const MainView = () => {
+    const allCounters =  useGenieSelector(() => {
+        return Counter.All(); //get all counters
+    });
+
+    return (
+        <View>
+            <View >
+                <button onClick={() => {
+                    //create a new counter to edit
+                    const counter = Counter.CreateCounter({name: "", type: "default",created: false});
+
+                    AppNavigator.push('EditCounter', counter)
+                }}>Add New Timer</button>
+            </View>
+
+            <CounterListView elements={allCounters}/>
+        </View>
+    )
+}
