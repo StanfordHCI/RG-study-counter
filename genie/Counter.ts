f4b13b111f729562b9ca52d2f1c0cb1d7d1e6327
Yingtian Shi <shiyt0313@gmail.com>
2023-08-24 20:13:13 +0000

    study user_11

---
+++ genie/Counter.ts
@@ -0,0 +1,71 @@
+import {GenieClass, DataClass, GenieProperty, int, GenieKey, GenieFunction} from "reactgenie-lib";
+
+@GenieClass("A counter example")
+export class Counter extends DataClass{
+    @GenieKey
+    public id: string;
+
+    @GenieProperty("The name of object that you want to count")
+    public name: string;
+    @GenieProperty("The type of objects (vegetable, fruit, ...)")
+    public type: string;
+    @GenieProperty() // You can omit the description
+    public count: int;
+    @GenieProperty() // You can omit the description
+    public created: boolean;
+
+    constructor({id, name, type, count = 0, created}: {id: string, name: string, type: string, count?: int, created:boolean}) {
+        super({})
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.count = count
+        this.created = created;
+    }
+
+    @GenieFunction()
+    static CreateCounter({name, type, count=0, created}:
+                             {name:string, type:string, count?:int, created:boolean}): Counter {
+        const id = Counter.All().length.toString(); // automatically generate a unique id
+        return Counter.CreateObject({id: id, name: name, type: type, count: count, created: created})
+    }
+
+    static setup() {
+        Counter.CreateCounter({name:"apple", type:"fruit",created:true});
+        Counter.CreateCounter({name:"orange", type:"fruit", created:true});
+        Counter.CreateCounter({name:"potato", type:"vegetable", created:true});
+    }
+
+    static Examples = [
+        {
+            user_utterance: "Increse this counter",
+            example_parsed: "Counter.Current().increment()",
+        },
+        {
+            user_utterance: "show me all vegetables counters",
+            example_parsed: 'Counter.All().matching(field: .type, value: "vegetable")',
+        },
+        {
+            user_utterance: "increase me all fruit counters",
+            example_parsed: 'Counter.All().matching(field: .type, value: "fruit").increment()',
+        },
+        {
+            user_utterance: "Delete this counter",
+            example_parsed: 'Counter.Current().delete()',
+        }]
+
+    @GenieFunction()
+    increment(): void {
+        this.count = this.count + 1;
+    }
+
+    @GenieFunction()
+    decrement(): void {
+        this.count -= 1;
+    }
+
+    @GenieFunction()
+    delete(): void {
+        Counter.DeleteObject({id: this.id});
+    }
+}
